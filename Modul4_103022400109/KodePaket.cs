e5f15965708c909d3baf1a2b0926b686fa7ccc1b
jiminbxbble <nailaturrahmah2006@gmail.com>
2026-03-25 08:05:03 +0800

    second commit

---
+++ Modul4_103022400109/KodePaket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+public class KodePaket
+{
+	public string getKodePaket(string namaPaket)
+	{
+		Dictionary<string, string> tabelPaket = new Dictionary<string, string>()
+		{
+			{"Basic", "P201"},
+			{"Standard", "P202"},
+			{"Premium", "P203"},
+			{"Unlimited", "P204"},
+			{"Gaming", "P205"},
+			{"Streaming", "P206"},
+			{"Family", "P207"},
+			{"Business", "P208"},
+			{"Student", "P209"},
+			{"Traveler", "P210"}
+		};
+
+		if (tabelPaket.ContainsKey(namaPaket)) {
+			return tabelPaket[namaPaket];
+		}
+		else { 
+			return "Tidak termasuk dalam daftar paket" 
+		}
+	}
+}
--- Modul4_103022400109/obj/Debug/net10.0/Modul4_103022400109.AssemblyInfo.cs
+++ Modul4_103022400109/obj/Debug/net10.0/Modul4_103022400109.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul4_103022400109")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f84db999848aae6100ddef1a27230edbbcebaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul4_103022400109")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul4_103022400109")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
